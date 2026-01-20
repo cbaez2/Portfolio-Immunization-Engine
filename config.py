7e90015b9c2946964705294afa4a9c80fcb325a0
Christopher Baez <chris_baez18@hotmail.com>
2026-01-19 17:56:53 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+# ===============================
+# USER-EDITABLE PARAMETERS
+# ===============================
+
+IMMUNIZATION_TYPE = "redington"   # "full" or "redington"
+
+i0 = 0.15
+
+liabilities = [1000, 1000, 1000, 1000]
+l_times     = [0, 2, 3, 4]
+
+a_times = [1,5]
+
+# Rebalancing
+i_n = 0.80
+t_n = 4.5
